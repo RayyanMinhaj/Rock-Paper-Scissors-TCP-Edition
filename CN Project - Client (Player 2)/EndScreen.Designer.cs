7cb63866663ccbdea8ed56d6d8e74e0da9aff862
RayyanMinhaj <rayyan20021@gmail.com>
2023-04-09 17:58:06 +0500

    first commit i guess :/

--- CN Project - Client (Player 2)/EndScreen.Designer.cs
+++ CN Project - Client (Player 2)/EndScreen.Designer.cs
@@ -70,7 +70,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(275, 73);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(0, 36);
